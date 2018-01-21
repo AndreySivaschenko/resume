f6d70ca4a41bffbb392a6588392a3de050ab383a
AndreySivaschenko <sivachenkoar@gmail.com>
2018-01-22 01:49:06 +0200

    resume

---
+++ index.php
@@ -0,0 +1,349 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link rel="stylesheet" type="text/css" href="style/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style/animate.css">
+	  <script src="js/wow.min.js"></script>
+	  <script>
+	    new WOW().init();
+	  </script>
+</head>
+<body>
+<header id="home">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 col-lg-12 head parallax-window" data-parallax="scroll" data-speed="0.3" data-image-src="/img/bg-head.png" >
+				<div class="wow bounceInDown">
+					<div class="header-menu">
+						<ul>
+							<li><a href="#home" data-target="anchor" >Home</a></li>
+							<li><a href="#about-me" data-target="anchor">About me</a>
+							<!--<li><a href="#works" class="active" data-target="anchor">My Works</a></li>-->
+							<li><a href="#contact" data-target="anchor">Contact me</a></li>
+						</ul>
+
+						<hr class="header-menu_border">
+					</div>
+				
+				</div>
+				<h1 class="header-text header-text__big wow slideInDown">Sivashchenko Andrey</h1>
+				<h3 class="header-text header-text__small wow slideInUp">Front-end developer</h3>
+				<div class="icon-block wow fadeIn" data-wow-delay="0.6s" style="display: none;">
+					<div class="header-icon">
+						<img class="header-icon-html" src="img/header_icon_html.png">
+						<img class="header-icon-css" src="img/header_icon_css.png">
+						<img class="header-icon-js" src="img/header_icon_js.png">
+						<img class="header-icon-bt" src="img/header_icon_bt.png">
+					</div>
+				</div>
+			</div>			
+		</div>
+	</div>
+</header>
+<section id="about-me">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="title-section">
+				<div class="border-title wow fadeIn">
+					<hr class="left-hr">
+						<h3>Who I am?</h3>
+					<hr class="right-hr">
+				</div>
+			</div>
+			<div class="selector-menu">
+				<div class="circle_1"></div>
+				<div class="circle_2 circle_active"></div>
+				<div class="circle_3"></div>
+			</div>
+				<div class="about-me-content">
+					<div class="about-me-photo wow fadeInDown"></div>
+						<div class="col-md-4 col-md-offset-2  about-me-text about-me-text__bio wow fadeInLeft">
+							<div class="title-bio">
+								<img src="img/icon.png">
+								<h2 class="title" id="bio-title-text"> My Biography</h2>
+							</div>
+							
+						
+						<hr>
+						<h5>Start</h5>
+						<p id="selectors__start"><img src='img/img03.png'></p>
+						<section class="section-about-me">
+							<span class="section-aboutMe-title">
+								<img class="bio-icon" src='img/icon-about-me-bio.png'>
+								<h3>About Me</h3>
+							</span>
+							<p class="center">Я Сиващенко Андрей Русланович 05.01.1996 года рождения. Родился в городе Соледар. С 11 класса заинтересовался веб-разработкой.На данный момент продолжаю развиваться в этой сфере. Мне интересно изучать что-то новое. Первый мой опыт по разработке веб-сайтов была разработка сайта-визитки для местного интернет-провайдера.
+							С 2013г учусь в Харьковском Национальном Университете Радиоэлектроники по специальности Программная Инженерия.
+							В университете был опыт по разработке андроид приложения, а также по одной из дисциплин разрабатывал интернет магазин. На летней практике проходил стажировку в фирме Digital Cloud Technologies, где разрабатывал шаблоны для Wordpress. На 4-м курсе университета на дипломную работу разрабатывал веб-приложение. Я использовал такие технологии как : Python, Django Framework, HTML5/CSS3, Bootstrap3, Javascript, Jquery и Аjax. В 2017г получил степень бакалавра по направлению Программная Инженерия. На данный момент учусь в Харьковском Национальном Университете Радиоэлектроники,получаю степень Магистра. Усусь по специальности: Project Manager.</p>
+						</section>
+
+						<p id="selectors"><img src='img/img06.png'></p>
+						
+						<section class="section-about-me">
+							<span class="section-aboutMe-title">
+								<img class="bio-icon" src='img/icon-university.png'>
+								<h3>University</h3>
+							</span>
+							<p>
+								<h4>01.09.2013 - 01.08.2017</h4>
+								<h6>Харьковский Национальный Университет Радиоэлектроники</h6>
+								<h4>Cпециальность</h4>
+								<h6>Программная Инженерия</h6>
+							</p>
+						</section>
+						<p id="selectors"><img src='img/img05.png'></p>
+
+						<section class="section-about-me">
+							<span class="section-aboutMe-title">
+								<img class="bio-icon" src='img/icon-hobbies-bio.png'>
+								<h3>Hobbies</h3>
+							</span>
+							<p class="center">Радиолюбитель, Электрогитара,Мотокросс.</p>
+						</section>
+
+						<p id="selectors"><img src='img/img06.png'></p>
+
+						<section class="section-about-me">
+							<span class="section-aboutMe-title">
+								<img class="bio-icon" src='img/icon-skills-bio.png'>
+								<h3 style="margin-left: 10px;">Experience</h3>
+							</span>
+							<div class="section-exp">
+								<h4>Разработка сайта под DLE для интернет провайдера SaltNet</h4>
+								<h6>Обязаность: web-design / html-css coder</h6>
+							</div>
+							
+							<div class="section-exp">
+								<h4>Проектная работа в университете, разработка интернет магазина</h4>
+								<h6>Обязаность: web-design / html-css coder / backend developer </h6>
+							</div>
+
+							<div class="section-exp">
+								<h4>Проектная работа в университете, разработка андроид приложения</h4>
+								<h6>Обязаность: Ui/Ux design / android developer </h6>
+							</div>
+
+							<div class="section-exp">
+							<h4>Летняя практика в Digital cloud technology</h4>
+							<h6>Обязаность: web-design / html-css coder под Wordpress</h6>
+							</div>
+
+							<div class="section-exp" id="section-exp__last">
+								<h4>Дипломная работа: Разработка веб-приложения "SkatchPad" </h4>
+								<h6>Обязаность: Full-stack developer</h6>
+								<h4>
+									Технологии которые использовались при выполнении дипломной работы: Python, Django Framework, HTML5/CSS3, Bootstrap 3, Javascript.
+								</h4>
+							</div>
+
+						</section>
+						<p id="selectors__finish"><img src='img/img04.png'></p>
+						<h5>End</h5>
+					</div>
+				<div class="col-md-4 col-md-offser-right-2  about-me-text about-me-text__skills wow fadeInUp">
+				<div class="title-bio">
+					<img src="img/skills-icon.png">
+					<h2 class="title"> My Skills</h2>
+				</div>
+					<div class="my-skillbar">
+						<div class="skillbar" data-percent="75%">
+							<div class="skillbar-title" style="background:#e55126;"><span>HTML5</span></div>
+							<div class="skillbar-bar" style="background:#e55126;"></div>
+							<div class="skillbar-bar-precent">75%</div>
+						</div>
+						<div class="skillbar" data-percent="70%">
+							<div class="skillbar-title" style="background:#00a8ff"><span>CSS3</span></div>
+							<div class="skillbar-bar" style="background:#00a8ff"></div>
+							<div class="skillbar-bar-precent">65%</div>
+						</div>
+						<div class="skillbar" data-percent="40%">
+							<div class="skillbar-title" style="background:#f2c126"><span>JS</span></div>
+							<div class="skillbar-bar" style="background:#f2c126"></div>
+							<div class="skillbar-bar-precent">40%</div>
+						</div>
+						<div class="skillbar" data-percent="60%">
+							<div class="skillbar-title" style="background:#161e4d"><span>Photoshop</span></div>
+							<div class="skillbar-bar" style="background:#161e4d"></div>
+							<div class="skillbar-bar-precent">60%</div>
+						</div>
+					</div>
+
+					<div class="title-bio">
+						<img src="img/download.png" class="download-icon">
+							<h2 class="title"> My Resume</h2>
+					</div>
+					<div class="resume-icon">
+						<h4 class="title-resume">Cкачать</h4>
+						<span class="download-resume">
+							<a href="#"><img src="img/doc-file.png"></a>
+						</span>
+
+						<span class="download-resume">
+							<a href="#"><img src="img/pdf-file.png"></a>
+						</span>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!--<section id="works">
+	<div class="container-fluid my-works parallax-window" data-parallax="scroll" data-speed="0.3" data-image-src="/img/bg-work.png">
+		<div class="row">
+			<div class="title-section-works ">
+				<div class="border-title-works wow fadeIn ">
+					<hr class="left-hr-works">
+						<h3>My Wokrs</h3>
+					<hr class="right-hr-works">
+				</div>
+			</div>
+			<div class="selector-menu">
+				<div class="circle_1 wow fadeInLeft"></div>
+				<div class="circle_2 circle_active"></div>
+				<div class="circle_3 wow fadeInRight"></div>
+			</div>
+			<div class="col-md-4 col-lg-4 img-center wow slideInDown">
+			<div class="grid">
+			<div class="border-works-image">
+				<figure class="effect-apollo">
+					<img src="img/works_1.png"/>
+						<figcaption>
+							<h2>Work<span>1</span></h2>
+							<p>Sivaschenko Andrey</p>
+							<a href="#">View more</a>
+						</figcaption>			
+						</figure>
+				</div>	
+				<div class="border-works-image">
+				<figure class="effect-apollo">
+					<img src="img/works_1.png"/>
+						<figcaption>
+							<h2>Work<span>4</span></h2>
+							<p>Sivaschenko Andrey</p>
+							<a href="#">View more</a>
+						</figcaption>			
+				</figure>
+				</div>
+			</div>
+			
+			</div>
+			<div class="col-md-4 col-lg-4 img-center wow fadeIn" data-wow-delay="0.2s">
+				<div class="grid">
+			<div class="border-works-image">
+				<figure class="effect-apollo">
+					<img src="img/works_1.png"/>
+						<figcaption>
+							<h2>Work<span>2</span></h2>
+							<p>Sivaschenko Andrey</p>
+							<a href="#">View more</a>
+						</figcaption>			
+						</figure>
+				</div>	
+				<div class="border-works-image">
+				<figure class="effect-apollo">
+					<img src="img/works_1.png"/>
+						<figcaption>
+							<h2>Work<span>5</span></h2>
+							<p>Sivaschenko Andrey</p>
+							<a href="#">View more</a>
+						</figcaption>			
+				</figure>
+				</div>
+			</div>
+			</div>
+
+			<div class="col-md-4 col-lg-4 img-center wow slideInUp" data-wow-delay="0.3s">
+				<div class="grid">
+			<div class="border-works-image">
+				<figure class="effect-apollo">
+					<img src="img/works_1.png"/>
+						<figcaption>
+							<h2>Work<span>3</span></h2>
+							<p>Sivaschenko Andrey</p>
+							<a href="#">View more</a>
+						</figcaption>			
+						</figure>
+				</div>	
+				<div class="border-works-image">
+				<figure class="effect-apollo">
+					<img src="img/works_1.png"/>
+						<figcaption>
+							<h2>Work<span>6</span></h2>
+							<p>Sivaschenko Andrey</p>
+							<a href="#">View more</a>
+						</figcaption>			
+				</figure>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
+</section>-->
+<footer id="contact">
+	<div class="container">
+		<div class="row">
+			<div class="title-section">
+				<div class="border-title border-title__footer wow fadeIn">
+					<hr class="left-hr left-hr__footer ">
+						<h3>Contact Me</h3>
+					<hr class="right-hr right-hr__footer">
+				</div>
+			</div>
+			<div class="selector-menu">
+				<div class="circle_1 circle-1__footer wow fadeInLeft"></div>
+				<div class="circle_2 circle-2__footer "></div>
+				<div class="circle_3 circle-3__footer circle_active__footer wow fadeInRight"></div>
+			</div>
+			<div class="col-md-6 footer-title">
+				<h2>My mobile phone</h2>
+				<h3>+380936060034</h3>
+
+				<h2>My Email</h2>
+				<h3>sivachenkoar@gmail.com</h3>
+			</div>
+			<!--<div class="col-md-4 footer-title">
+				<h2>Write Me</h2>
+				<form method="POST" action="submit.php" class="send-me">
+					<input type="text" name="Name" placeholder="Name">
+					<input type="email" name="Email" placeholder="Email">
+					<textarea placeholder="Messages"></textarea>
+					<div class="send-button">
+							<hr class="send-button__lefthr">
+							<input id="submit" type="submit" value="Send">
+							<hr class="send-button__righthr">
+					</div>
+
+					<div class="copyright">
+						<h4>Sivashchenko Andrey © 2017</h4>
+					</div>
+				</form>
+			</div>-->
+			<div class="col-md-6 footer-title">
+				<h2 id="soc-title">Social Networks</h2>
+				<div class="social">
+					<a href="#" class="soc-vk">&nbsp;</a>
+					<a href="#" class="soc-fb">&nbsp;</a>
+					<a href="#" class="soc-g">&nbsp;</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/parallax.min.js"></script>
+<script src="http://code.jquery.com/jquery-3.2.1.min.js"
+		integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+		crossorigin="anonymous"></script>
+<script src="js/skills.js"></script>
+<script src="js/scroll.js"></script>
+</body>
+</html>
+
+
